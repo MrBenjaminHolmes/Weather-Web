799a464ff2537e264d22c00a4767294b4cfeda62
Ben Holmes <149116719+MrFarmerBon@users.noreply.github.com>
2024-10-29 19:51:16 +0000

    Made UI and Tacking

--- src/index.js
+++ src/index.js
@@ -1,17 +1,26 @@
 import "./styles.css";
-
+const input = document.getElementById("location");
 function getData(location) {
   console.log("Fetching Data...");
   fetch(
-    "https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/wrexham?key=BQDWT4GT3GAZY8Z364V8BU4JH",
+    `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}?key=BQDWT4GT3GAZY8Z364V8BU4JH`, // Use actual key
     { mode: "cors" }
   )
     .then(function (response) {
       return response.json();
     })
-    .then(function (response) {
-      console.log(response.days);
+    .then(function (data) {
+      console.log(data);
+      console.log(data.days);
     });
 }
 
-getData("Chester");
+// Call the function
+
+input.addEventListener("keydown", function (event) {
+  // Check if the Enter key was pressed
+  if (event.key === "Enter") {
+    // Call getData with the input value
+    getData(input.value);
+  }
+});
