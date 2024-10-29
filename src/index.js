b9536549a1880f83ca5eaf6a88d89596fac4d42a
Ben Holmes <149116719+MrFarmerBon@users.noreply.github.com>
2024-10-29 13:30:54 +0000

    Reading Data for 2 Weeks

--- src/index.js
+++ src/index.js
@@ -2,6 +2,16 @@ import "./styles.css";
 
 function getData(location) {
   console.log("Fetching Data...");
+  fetch(
+    "https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/wrexham?key=BQDWT4GT3GAZY8Z364V8BU4JH",
+    { mode: "cors" }
+  )
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (response) {
+      console.log(response.days);
+    });
 }
 
 getData("Chester");
