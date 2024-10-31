ec6b031d1bc637bc590f90a0982834e90d1f8b40
Ben Holmes <149116719+MrFarmerBon@users.noreply.github.com>
2024-10-31 15:28:28 +0000

    Finished Program

--- src/index.js
+++ src/index.js
@@ -7,25 +7,21 @@ window.addEventListener("load", function () {
 
 const input = document.getElementById("location");
 
-function getData(location) {
-  console.log("Fetching Data...");
+async function getData(location) {
   const cardContainer = document.getElementById("carousel");
-  cardContainer.innerHTML = "<p>Loading data...</p>";
+  cardContainer.innerHTML = "";
 
-  fetch(
+  const response = await fetch(
     `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}?key=BQDWT4GT3GAZY8Z364V8BU4JH`,
     { mode: "cors" }
-  )
-    .then(function (response) {
-      return response.json();
-    })
-    .then(function (data) {
-      displayCards(data.days[0].hours);
-      console.log(data);
-      displayCurrent(data);
-      initCarousel();
-    });
+  );
+
+  const data = await response.json();
+  displayCards(data.days[0].hours);
+  displayCurrent(data);
+  initCarousel();
 }
+
 function displayCurrent(data) {
   const mainContainer = document.getElementById("info");
 
@@ -41,8 +37,10 @@ function displayCurrent(data) {
         <div class="feelslike">Feels Like:${Math.round(
           (data.currentConditions.feelslike - 32) * (5 / 9)
         )}°C</div>
-        <div class="wind">Wind:${data.currentConditions.windspeed}</div>
-        <div class="humidity">${data.currentConditions.humidity}:10%</div>
+        <div class="wind">Wind Speed:${
+          data.currentConditions.windspeed
+        }mph</div>
+        <div class="humidity">Humidity:${data.currentConditions.humidity}%</div>
         <div class="cloudcover">Cloud Cover: ${
           data.currentConditions.cloudcover
         }%</div>
@@ -58,16 +56,16 @@ function displayCards(data) {
     const cardDiv = document.createElement("div");
     cardDiv.classList.add("card");
     cardDiv.innerHTML = `
-      <h2>${hour.datetime}</h2>
-      <h1>${Math.round((hour.temp - 32) * (5 / 9))}°C</h1>
+      <h2 class = "toobig">${hour.datetime}</h2>
+      <h1  class = "toobig">${Math.round((hour.temp - 32) * (5 / 9))}°C</h1>
       <div>
-        <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#FFFFFF">
+        <svg  xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#FFFFFF">
           <path d="M460-160q-50 0-85-35t-35-85h80q0 17 11.5 28.5T460-240q17 0 28.5-11.5T500-280q0-17-11.5-28.5T460-320H80v-80h380q50 0 85 35t35 85q0 50-35 85t-85 35ZM80-560v-80h540q26 0 43-17t17-43q0-26-17-43t-43-17q-26 0-43 17t-17 43h-80q0-59 40.5-99.5T620-840q59 0 99.5 40.5T760-700q0 59-40.5 99.5T620-560H80Z"/>
         </svg>
         <p>Wind Speed: ${hour.windspeed} mph</p>
       </div>
       <div>
-        <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#FFFFFF"><path d="M720-200q-17 0-28.5-11.5T680-240q0-17 11.5-28.5T720-280q17 0 28.5 11.5T760-240q0 17-11.5 28.5T720-200ZM280-80q-17 0-28.5-11.5T240-120q0-17 11.5-28.5T280-160q17 0 28.5 11.5T320-120q0 17-11.5 28.5T280-80Zm-40-120q-17 0-28.5-11.5T200-240q0-17 11.5-28.5T240-280h360q17 0 28.5 11.5T640-240q0 17-11.5 28.5T600-200H240ZM400-80q-17 0-28.5-11.5T360-120q0-17 11.5-28.5T400-160h280q17 0 28.5 11.5T720-120q0 17-11.5 28.5T680-80H400ZM300-320q-91 0-155.5-64.5T80-540q0-83 55-145t136-73q32-57 87.5-89.5T480-880q90 0 156.5 57.5T717-679q69 6 116 57t47 122q0 75-52.5 127.5T700-320H300Zm0-80h400q42 0 71-29t29-71q0-42-29-71t-71-29h-60v-40q0-66-47-113t-113-47q-48 0-87.5 26T333-704l-10 24h-25q-57 2-97.5 42.5T160-540q0 58 41 99t99 41Zm180-200Z"/></svg>
+        <svg xmlns="http://www.w3.org/2000/svg" class = "toobig" height="24px" viewBox="0 -960 960 960" width="24px" fill="#FFFFFF"><path d="M720-200q-17 0-28.5-11.5T680-240q0-17 11.5-28.5T720-280q17 0 28.5 11.5T760-240q0 17-11.5 28.5T720-200ZM280-80q-17 0-28.5-11.5T240-120q0-17 11.5-28.5T280-160q17 0 28.5 11.5T320-120q0 17-11.5 28.5T280-80Zm-40-120q-17 0-28.5-11.5T200-240q0-17 11.5-28.5T240-280h360q17 0 28.5 11.5T640-240q0 17-11.5 28.5T600-200H240ZM400-80q-17 0-28.5-11.5T360-120q0-17 11.5-28.5T400-160h280q17 0 28.5 11.5T720-120q0 17-11.5 28.5T680-80H400ZM300-320q-91 0-155.5-64.5T80-540q0-83 55-145t136-73q32-57 87.5-89.5T480-880q90 0 156.5 57.5T717-679q69 6 116 57t47 122q0 75-52.5 127.5T700-320H300Zm0-80h400q42 0 71-29t29-71q0-42-29-71t-71-29h-60v-40q0-66-47-113t-113-47q-48 0-87.5 26T333-704l-10 24h-25q-57 2-97.5 42.5T160-540q0 58 41 99t99 41Zm180-200Z"/></svg>
         <p>Cloud Cover: ${hour.cloudcover}%</p>
       </div>
       <div>
